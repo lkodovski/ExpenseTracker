7f24d2080b9ab88946d63367a4d5c8f12254f5e5
lkodovski <l.kodovski@hotmail.com>
2023-04-07 14:21:48 +0200

    First commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+export default function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28th</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
+      </div>
+    </div>
+  );
+}
