4cc7c9eef1c76f2fbf5a924c2b2fea0343f9b8a6
lkodovski <l.kodovski@hotmail.com>
2023-04-27 08:20:42 +0200

    Array od Object

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
-  const expenseDate = new Date();
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+export default function ExpenseItem(props) {
+  // const expenseDate = new Date();
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 294.67;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
